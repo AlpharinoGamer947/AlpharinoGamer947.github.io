993338b6a3fae112b20fc395700f3f48b16c428b
AlpharinoGamer947 <AlpharinoGamer947@gmail.com>
2024-10-22 10:45:34 -0700

    Website Version 7 Imported

---
+++ js/tags.js
@@ -0,0 +1,10 @@
+document.addEventListener('DOMContentLoaded', function () {
+    // Red Color
+    const disclaimer = document.getElementById('disclaimer')
+
+    // h4 element
+    const text = document.createElement('h4')
+
+    text.textContent = 'Disclaimer: Website is Primitive'
+    disclaimer.appendChild(text)
+})
