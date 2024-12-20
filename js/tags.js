cba2fbafedf9c7554f1a88c367c1bca9ff9ee463
AlpharinoGamer947 <AlpharinoGamer947@gmail.com>
2024-12-19 22:37:07 -0800

    readded tags.js

---
+++ js/tags.js
@@ -0,0 +1,10 @@
+document.addEventListener("DOMContentLoaded", function () {
+  // Red Color
+  const disclaimer = document.getElementById("disclaimer");
+
+  // h4 element
+  const text = document.createElement("h4");
+
+  text.textContent = "Disclaimer: This website is primitive";
+  disclaimer.appendChild(text);
+});
