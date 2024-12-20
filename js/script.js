a935f89438bff8684b8b782b6e47c99c40147aab
AlpharinoGamer947 <AlpharinoGamer947@gmail.com>
2024-12-19 22:29:18 -0800

    adding link to internship

--- js/script.js
+++ js/script.js
@@ -1,48 +1,48 @@
+document.addEventListener("DOMContentLoaded", function () {
+  // console.log("DOMContentLoaded entered");
 
-document.addEventListener('DOMContentLoaded', function () {
-    // console.log("DOMContentLoaded entered");
+  // create navbar tag
+  const navbar = document.getElementById("navbar");
 
-    // create navbar tag
-    const navbar = document.getElementById('navbar');
+  // create navbar container
+  const container = document.createElement("div");
+  container.className = "container";
 
-    // create navbar container
-    const container = document.createElement('div');
-    container.className = 'container';
+  // add container to navbar
+  navbar.appendChild(container);
 
-    // add container to navbar
-    navbar.appendChild(container);
+  // the links in the navigation bar
+  // all in an array
+  const navlinks = [
+    { text: "Home", link: "/index.html" },
+    { text: "VG", link: "/html/videogames/videogames.alpharinogamer947.html" },
+    { text: "Blender", link: "/html/blender/blender.alpharinogamer947.html" },
+    { text: "Notes", link: "/html/test.html" },
+    { text: "Internship", link: "/html/internships/login.html" },
+  ];
 
-    // the links in the navigation bar
-    // all in an array
-    const navlinks = [
-        { text: 'Home', link: '/index.html' },
-        { text: 'VG', link: '/html/videogames/videogames.alpharinogamer947.html' },
-        { text: 'Blender', link: '/html/blender/blender.alpharinogamer947.html' },
-        { text: 'Notes', link: '/html/test.html' }
-    ]
+  // for each loop
+  navlinks.forEach((item) => {
+    // create a div for each of the links
+    const navitem = document.createElement("div");
+    navitem.className = "navlink";
 
-    // for each loop
-    navlinks.forEach(item => {
+    // create link for each item
+    const link = document.createElement("a");
+    link.textContent = item.text; // set text to div
+    link.href = item.link; // set hyper-link
 
-        // create a div for each of the links
-        const navitem = document.createElement('div');
-        navitem.className = 'navlink';
+    // add link to div
+    navitem.appendChild(link);
 
-        // create link for each item
-        const link = document.createElement('a');
-        link.textContent = item.text; // set text to div
-        link.href = item.link; // set hyper-link
-
-        // add link to div
-        navitem.appendChild(link);
-
-        // adds item to container
-        container.appendChild(navitem);
-    });
+    // adds item to container
+    container.appendChild(navitem);
+  });
 });
 
-// 
-{/* <div class="container">
+//
+{
+  /* <div class="container">
         <a href="index.html" class="navlink">
             <div>Home</div>
         </a>
@@ -50,4 +50,5 @@ document.addEventListener('DOMContentLoaded', function () {
             <div>VG</div>
         </a>
     </div> 
-*/}
+*/
+}
